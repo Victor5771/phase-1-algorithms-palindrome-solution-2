decbf7622d2dedda4552380b10dc15e60bdca91a
Victor <victor.kyalo@student.moringaschool.com>
2023-10-05 17:39:56 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      
+      return false;
+    }
+  }
+
+  
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  Function isPalindrome(word):
+  Initialize startIndex to 0
+  Initialize endIndex to the length of word - 1
+
+  While startIndex < endIndex:
+    If word[startIndex] is not equal to word[endIndex]:
+      Return false
+    Increment startIndex
+    Decrement endIndex
+
+  Return true
+
+
 */
 
 /*
   Add written explanation of your solution here
+  The isPalindrome function checks whether a given word is a palindrome,
+   which means it reads the same forwards and backwards.
+    It does this by comparing characters from both ends of the word towards the middle.
+     If any characters don't match during this comparison, 
+     it returns false, indicating the word is not a palindrome.
+    If all characters match as it reaches the middle, 
+      it returns true, confirming that the word is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
